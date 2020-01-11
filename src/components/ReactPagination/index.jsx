9ae45f711e85860646fe12a181475712e05f9308
Sumit Jadhav <summit.jadhav@gmail.com>
2020-01-11 22:31:22 +0530

    Created initial project setup using Neutrino

---
+++ src/components/ReactPagination/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <button type="button">Prev</button>
+    <button type="button">1</button>
+    <button type="button">2</button>
+    <button type="button">3</button>
+    <button type="button">4</button>
+    <button type="button">Next</button>
+  </div>
+);
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import ReactPagination from './components/ReactPagination';
+
+render(<ReactPagination />, document.getElementById('root'));
